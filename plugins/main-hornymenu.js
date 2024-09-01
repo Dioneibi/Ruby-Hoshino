88009e202c311d3521c6dd16dd38ff2924347d8a
Dioneibi <ulcidecono@gmail.com>
2024-09-01 16:17:38 -0400

    Update main-hornymenu.js

--- plugins/main-hornymenu.js
+++ plugins/main-hornymenu.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `⚠️ ¡𝐢𝐝𝐢𝐨𝐭𝐚, 𝐞𝐬𝐭𝐨𝐬 𝐜𝐨𝐦𝐚𝐧𝐝𝐨𝐬 𝐞𝐬𝐭𝐚𝐧 𝐝𝐞𝐬𝐚𝐜𝐭𝐢𝐯𝐚𝐝𝐨𝐬, 𝐦𝐞 𝐝𝐚𝐬 𝐚𝐬𝐜𝐨 𝐦𝐞𝐣𝐨𝐫 𝐩𝐢𝐞𝐫𝐝𝐞𝐭𝐞!`; 
   try {
-    const pp = 'https://telegra.ph/file/508f8a2f56302590f8c12.jpg';
+    const pp = imagen4;
     // let vn = './media/menu.mp3'
     const d = new Date(new Date + 3600000);
     const locale = 'es';
