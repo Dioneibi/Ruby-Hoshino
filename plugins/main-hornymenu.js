98dba92198368edf7464588a1962a6d80be76015
Dioneibi <ulcidecono@gmail.com>
2024-09-01 13:04:37 -0400

    Update main-hornymenu.js

--- plugins/main-hornymenu.js
+++ plugins/main-hornymenu.js
@@ -1,25 +1,28 @@
 import fetch from 'node-fetch';
+
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
-  if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `💥 *¡Estos comandos están desactivados!*`; 
-  try {
-    const pp = imagen3;
-    // let vn = './media/menu.mp3'
-    const d = new Date(new Date + 3600000);
-    const locale = 'es';
-    const week = d.toLocaleDateString(locale, {weekday: 'long'});
-    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
-    const _uptime = process.uptime() * 1000;
-    const uptime = clockString(_uptime);
-    const user = global.db.data.users[m.sender];
-    const {money, joincount} = global.db.data.users[m.sender];
-    const {exp, estrellas, level, role} = global.db.data.users[m.sender];
-    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
-    const more = String.fromCharCode(8206);
-    const readMore = more.repeat(850);
-    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
-    const document = doc[Math.floor(Math.random() * doc.length)];
-        const str = `*╭━━━━•『  MENU +🔞 』•━━━━╮*
+  if (!db.data.chats[m.chat].modohorny && m.isGroup) {
+    const audioUrl = 'https://www.mediafire.com/file/1jwozo3z8yatmxw/Me_das_asco_pierdete_opm_meme_plantilla/file';
+    const response = await fetch(audioUrl);
+    const buffer = await response.buffer();
+    try {
+      const pp = imagen3;
+      const d = new Date(new Date + 3600000);
+      const locale = 'es';
+      const week = d.toLocaleDateString(locale, {weekday: 'long'});
+      const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+      const _uptime = process.uptime() * 1000;
+      const uptime = clockString(_uptime);
+      const user = global.db.data.users[m.sender];
+      const {money, joincount} = global.db.data.users[m.sender];
+      const {exp, estrellas, level, role} = global.db.data.users[m.sender];
+      const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+      const more = String.fromCharCode(8206);
+      const readMore = more.repeat(850);
+      const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+      const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+      const document = doc[Math.floor(Math.random() * doc.length)];
+      const str = `*╭━━━━•『  MENU +🔞 』•━━━━╮*
 *│╭─────━───────━────*
 ││ ◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸🔞 _.pack_
 ││ ◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸🔞 _.pack2_
@@ -55,22 +58,23 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ││ ◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸🔞 _.hentaisearch *<texto>*_
 *│╰人人人人人人人人人人人人╯*
 *╰─ - ✦⢄⢁✩*⢄⢁🔞⡠*✩⡈⡠✦ - ─╯*`.trim();
-    if (m.isGroup) {
-      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
-      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
-    } else {
-      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
-      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'}};
+      if (m.isGroup) {
+        conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+        conn.sendMessage(m.chat, { audio: { url: audioUrl }, mimetype: 'audio/mp4' }, {quoted: fkontak2});
+      } else {
+        conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+        conn.sendMessage(m.chat, { audio: { url: audioUrl }, mimetype: 'audio/mp4' }, {quoted: fkontak2});
+      }
+    } catch {
+      conn.reply(m.chat, '💥 *¡Ocurrió Un Error!*', m);
     }
-  } catch {
-    conn.reply(m.chat, '💥 *¡Ocurrió Un Error!*', m);
   }
 };
+
 handler.tags = ['main']
 handler.help = ['hornymenu']
-handler.command = /^(menuhorny|hornymenu)$/i;
+handler.command = ['menuhorny', 'hornymenu'];
 handler.exp = 50;
 handler.fail = null;
 export default handler;
@@ -79,4 +83,5 @@ function clockString(ms) {
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
   return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
-}
+        }
+      
