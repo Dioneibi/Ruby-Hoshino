573e21bd28ea2d89dd09ced9dfa5360f015b3405
Dioneibi <ulcidecono@gmail.com>
2024-09-03 17:59:44 -0400

    Update config.js

--- config.js
+++ config.js
@@ -64,7 +64,7 @@ global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
 global.wm2 = `${dia} ${fecha}\nLuffy`;
-global.nomorown = '18294868853;
+global.nomorown = '18294868853';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
