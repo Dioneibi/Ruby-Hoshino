b23f27222c7e70903a0df7d51833e9e4f925320c
Dioneibi <ulcidecono@gmail.com>
2024-09-15 18:07:54 -0400

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -4,61 +4,76 @@ import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
 
 let tags = {
-  'main': 'INFO 📚',
-  'buscador': 'BUSQUEDAS 🔎',
-  'fun': 'JUEGOS 🎮',
-  'serbot': 'SUB BOTS 🤖',
-  'rpg': 'RPG 🌠',
-  'rg': 'REGISTRO 📁',
-  'xp': 'EXP 🏷',
-  'sticker': 'STICKERS 🏞',
-  'anime': 'ANIMES 🍧',
-  'database': 'DATABASE ✨️',
-  'fix': 'FIXMSGESPERA 💭',
-  'grupo': 'GRUPOS 👥',
-  'nable': 'ON / OFF 📴', 
-  'descargas': 'DESCARGAS 📥',
-  'tools': 'HERRAMIENTAS 🔧',
-  'info': 'INFORMACIÓN 🐢',
-  'nsfw': 'NSFW 🔞', 
-  'owner': 'CREADOR 👑', 
-  'audio': 'AUDIOS 🔉', 
-  'ai': 'AI 🌹',
-  'transformador': 'CONVERTIDORES 🚩',
+  'main': '𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈Ó𝐍 📘',
+  'buscador': '𝐁𝐔𝐒𝐐𝐔𝐄𝐃𝐀𝐒 🔍',
+  'fun': '𝐉𝐔𝐄𝐆𝐎𝐒 🎲',
+  'jadibot': '𝐒𝐮𝐛𝐁𝐨𝐭/𝐁𝐨𝐭𝐬 🤖',
+  'rpg': '𝐑𝐏𝐆 🎲',
+  'rg': '𝐑𝐄𝐆𝐈𝐒𝐓𝐑𝐎 📂',
+  'xp': '𝐄𝐗𝐏𝐄𝐑𝐈𝐄𝐍𝐂𝐈𝐀 🏆',
+  'sticker': '𝐒𝐓𝐈𝐂𝐊𝐄𝐑𝐒 📑',
+  'anime': '𝐀𝐍𝐈𝐌𝐄 🍡',
+  'database': '𝐁𝐀𝐒𝐄 𝐃𝐄 𝐃𝐀𝐓𝐎𝐒 💾',
+  'fix': '𝐀𝐑𝐑𝐄𝐆𝐋𝐎𝐒 🛠️',
+  'grupo': '𝐆𝐑𝐔𝐏𝐎𝐒 🗣️',
+  'nable': '𝐎𝐍 / 𝐎𝐅𝐅 📴',
+  'descargas': '𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀𝐒 ⬇️',
+  'youtube': '𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐏𝐋𝐀𝐘 🎥',
+  'tools': '𝐇𝐄𝐑𝐑𝐀𝐌𝐈𝐄𝐍𝐓𝐀𝐒 🧰',
+  'info': '𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈Ó𝐍 📝',
+  'nsfw': '𝐍𝐒𝐅𝐖 🔞',
+  'owner': '𝐂𝐑𝐄𝐀𝐃𝐎𝐑 👑',
+  'mods': '𝐒𝐓𝐀𝐅𝐅 🌟',
+  'audio': '𝐀𝐔𝐃𝐈𝐎𝐒 🎶',
+  'ai': '𝐈𝐀 🤖',
+  'transformador': '𝐂𝐎𝐍𝐕𝐄𝐑𝐓𝐈𝐃𝐎𝐑𝐄𝐒 ⚙️'
 }
 
 const defaultMenu = {
-  before: `*─ׄ─ׅ─⭒─ׄ─ׄ─⭒─ׅ─ׄ─⭒─ׄ─ׄ─⭒─ׄ─ׄ─*
+  before: `｡☆✼★━━━━━━━━━━━━━━━━━★✼☆｡
+        ┎┈┈┈┈┈┈┈୨♡୧┈┈┈┈┈┈┈┒
+    𓏲꯭֟፝੭ ꯭⌑(꯭𝐑).꯭𝐔.꯭𝐁.꯭𝐘.꯭ ⭑𝐇.꯭𝐎.꯭𝐒.꯭𝐇.꯭𝐈.꯭𝐍.꯭𝐎.꯭𓏲꯭֟፝੭ 
+        ┖┈┈┈┈┈┈┈୨♡୧┈┈┈┈┈┈┈┚
+｡☆✼★━━━━━━━━━━━━━━━━━★✼☆｡
 
-“ Hola *%name* soy *Ai-Yaemori*, %greeting ”
 
-╭────═[ *INFO - BOT* ]═─────⋆
-│╭───────────────···
-┴│✯ 🍟 *Bot:* Ai Yaemori - MD 
-✩│✯ 🍂 *Modo* Público
-✩│✯ ✨️ *Baileys:* Multi Device
-✩│✯ ⌛️ *Tiempo Activo:* %muptime
-┬│✯ 🫂 *Usuarios:* %totalreg
-│╰────────────────···
-╰────────═┅═─────────
+"¡Hola, %name! Mi nombre es *Ruby Hoshino*"
+
+╔═══════⩽✦✰✦⩾═══════╗
+       「 𝙄𝙉𝙁𝙊 𝘿𝙀𝙇 𝘽𝙊𝙏 」
+╚═══════⩽✦✰✦⩾═══════╝
+║ ☆ 🌟 *𝖳𝖨𝖯𝖮 𝖣𝖤 𝖡𝖮𝖳*: *𝖶𝖠𝖨𝖥𝖴*
+║ ☆ 🚩 *𝖬𝖮𝖣𝖮*: *𝖯𝖴𝖡𝖫𝖨𝖢𝖮*
+║ ☆ 📚 *B𝖠𝖨𝖫𝖤𝖸𝖲*: *𝖬𝖴𝖫𝖳𝖨 𝖣𝖤𝖵𝖨𝖢𝖤*
+║ ☆ ⏱️ *𝖳𝖨𝖤𝖬𝖯𝖮* *𝖠𝖢𝖳𝖨𝖵𝖮*: %muptime
+║ ☆ 👤 *𝖴𝖲𝖴𝖠𝖱𝖨𝖮𝖲* *𝖱𝖤𝖦𝖨𝖲𝖳𝖱𝖠𝖣𝖮𝖲*: %totalreg
+║ ☆ 👩‍💻 *𝖢𝖱𝖤𝖠𝖣𝖮𝖱*: [𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑](https://Wa.me/18294868853)
+╚════════════════════════╝
+
+╔═══════⩽✦✰✦⩾═══════╗
+     「 𝙄𝙉𝙁𝙊 𝘿𝙀𝙇 𝙐𝙎𝙐𝘼𝙍𝙄𝙊 」
+╚═══════⩽✦✰✦⩾═══════╝
+║ ☆  🌐 *𝖢𝖫𝖨𝖤𝖭𝖳𝖤*: %name
+║ ☆ 🚀 *𝖤𝖷𝖯𝖤𝖱𝖨𝖤𝖭𝖢𝖨𝖠*: %exp
+║ ☆ ✨ *𝖤𝖲𝖳𝖱𝖤𝖫𝖫𝖠𝖲*: %estrellas
+║ ☆ 📊 *𝖭𝖨𝖵𝖤𝖫*: %level
+║ ☆ 🏅 *𝖱𝖠𝖭𝖦𝖮*: %role
+╚═══════════════════════╝
+
+╔═══════⩽✦✰✦⩾═══════╗
+    「 𝙎𝙪𝙗𝘽𝙤𝙩 / 𝘽𝙤𝙩 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 」
+╚═══════⩽✦✰✦⩾═══════╝
+║ ☆ %botofc 
+╚═════════════════════✦
+
 %readmore
-╭────═[ *INFO - USER* ]═─────⋆
-│╭───────────────···
-┴│✯ 🚩 *Cliente:* %name
-✩│✯ 💥 *Exp:* %exp
-✩│✯ 🌟 *Estrellas:* %estrellas
-✩│✯ 🐢 *Nivel:* %level
-┬│✯ ⚓ *Rango:* %role
-│╰────────────────···
-╰────────═┅═─────────
-%readmore
-*─ׄ─ׄ─⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ─⭒─ׄ─ׄ─⭒─ׄ─ׅ─*
+*☆─ׅ─ׄ★─ׅ─ׄ✮─ׅ─ׄ★─ׅ─ׄ☆─ׅ─ׄ☆─ׅ─ׄ★─ׅ─ׄ✮─ׅ─ׄ★─ׅ─ׄ☆*
 
 \t*L I S T A  -  D E  -  C O M A N D O S* 
 `.trimStart(),
-      header: '╭───═[ %category ]═────⋆\n│╭───────────────···',
-  body: '││ %cmd\n',
-  footer: '│╰────────────────···\n╰────────═┅═─────────\n',
+      header: '.    ╔═══════ • ° ❁⊕❁ ° • ═══════╗\n╭╼☁️✿⃟⃢᭄͜═✩═[%category]═✩═⃟⃢᭄͜✿☁️\n┃֪࣪  ╚═══════ • ° ❁⊕❁ ° • ═══════╝',
+  body: '├ׁ̟̇˚₊· ͟͟͞͞➳❥ %cmd\n',
+  footer: '╚▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬ִ▭࣪▬▭╝\n',
   after: `> ${dev}`,
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
@@ -160,21 +175,54 @@ const who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ?
 
 const pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/327f6ad853cb4f405aa80.jpg')
 
-//await conn.reply(m.chat, '*✰ Cargando Menú. . .*', m, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: packname, body: team, sourceUrl: channel, thumbnail: icons }}})
+  let category = "video"
+  const db = './media/database/db.json'
+  const db_ = JSON.parse(fs.readFileSync(db))
+  const random = Math.floor(Math.random() * db_.links[category].length)
+  const rlink = db_.links[category][random]
+  global.vid = rlink
+  const response = await fetch(vid)
+  const gif = await response.buffer()
 
-await m.react('⭐️') 
+    // Define rcanal here
+    let rcanal = "https://chat.whatsapp.com/KBQZ7fAdAY3HtVudDl3Cmy";
+  
+  
+    await conn.reply(m.chat, '*ꪹ͜𓂃⌛͡𝗘𝗻𝘃𝗶𝗮𝗻𝗱𝗼 𝗠𝗲𝗻𝘂 𝗱𝗲𝗹 𝗕𝗼𝘁....𓏲੭*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: packname, body: '%greeting', sourceUrl: redes, thumbnail: icons }}})
+
+// await conn.reply(m.chat, '🍟 Enviando el menú.....', m, rcanal)
+
+await m.react('💖') 
 
 //await conn.sendFile(m.chat, imagen1, 'yaemori.jpg', text.trim(), fkontak, null, rcanal)
 
-conn.sendMessage(m.chat, {text: text.trim(), mentions: [...text.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: { mentionedJid: [...text.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": packname, body: team, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen1, "mediaUrl": global.channel, "sourceUrl": global.channel}}}, {quoted: m});
+await conn.sendMessage(
+  m.chat,
+  { video: { url: vid }, caption: text.trim(),
+  contextInfo: {
+    mentionedJid: [m.sender],
+    isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: '120363263466636910@newsletter',
+      newsletterName: '『✯ Team Channe✯』',
+      serverMessageId: -1,
+    },
+    forwardingScore: 999,
+    externalAdReply: {
+      title: 'ᶻ 𝗓 𐰁✰ŕüḅÿ ħόşħίήό✰🦋⃤.ᐟ ֹ ₊ ꒱',
+      body: dev,
+      thumbnailUrl: icono,
+      sourceUrl: redes,
+      mediaType: 1,
+      renderLargerThumbnail: false,
+    },
+  },
 
-/*await conn.sendButton(m.chat, text, team, imagen1, [
-['𝗜𝗡𝗦𝗧𝗔𝗟𝗔𝗥 𝗕𝗢𝗧 🤖', '.instalarbot'],
-['𝗘𝗦𝗧𝗔𝗗𝗢 🪐', '#status'],
-['𝗚𝗥𝗨𝗣𝗢𝗦 👥', '#gruposai']], null, null, fkontak)*/
+  gifPlayback: true, gifAttribution: 0 },
+  { quoted: fkontak })
 
   } catch (e) {
-    conn.reply(m.chat, '❌️ Lo sentimos, el menú tiene un error', m, rcanal, )
+    conn.reply(m.chat, '🔵 Lo sentimos, el menú tiene un error', m, rcanal, )
     throw e
   }
 }
@@ -195,8 +243,8 @@ function clockString(ms) {
   return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
 
-  var ase = new Date();
-  var hour = ase.getHours();
+var ase = new Date();
+var hour = ase.getHours();
 switch(hour){
   case 0: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌙'; break;
   case 1: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 💤'; break;
@@ -223,4 +271,6 @@ switch(hour){
   case 22: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌙'; break;
   case 23: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌃'; break;
 }
-  var greeting = hour;
+var greeting = hour;
+
+    
