1b6112fad83ed25494950961e635bba8dc43ffa4
Dioneibi <ulcidecono@gmail.com>
2024-09-15 14:13:45 -0400

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -95,7 +95,7 @@ global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: `${packname}`, orderTitle: 'Bang', thumbnail: icons, sellerJid: '0@s.whatsapp.net'}}}
 
 global.icono = [ 
-'https://telegra.ph/file/bc69b423fafa4ceb9f54e.jpg',
+'https://qu.ax/tFuy.jpeg',
 'https://telegra.ph/file/c44a34dc20be35d01fc2e.jpg',
 'https://telegra.ph/file/4b386b0d3a4c346cf19be.jpg',
 'https://telegra.ph/file/bb82643dce0f91bb1b6f5.jpg',
