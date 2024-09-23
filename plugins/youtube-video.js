6cd2216bcce4afe4ce398ca17bb4f7f23dab2caf
Dioneibi <ulcidecono@gmail.com>
2024-09-22 23:54:16 -0400

    Update youtube-video.js

--- plugins/youtube-video.js
+++ plugins/youtube-video.js
@@ -13,12 +13,12 @@ let q = urls[1] || '480p'
 try {
 await m.react(rwait)
 const yt = await fg.ytv(urls[0], q)
-let { title, dl_url, size } = yt 
+let { title, dl_url, size, author, views, duration, published, id } = yt 
 
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `🚩 El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `🚩 El archivo pesa más de ${limit} MB, se canceló la descarga.`, m, rcanal) 
 
 conn.reply(m.chat, `🕒 *Descargando El Video*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
+contextInfo: { externalAdReply: { mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
 previewType: 0, thumbnail: icons,
@@ -26,43 +26,42 @@ sourceUrl: channel }}})
 await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `_*DESCARGAS - MEGUMIN 🔥*_
 ╭───────┈♡┈──────
 │𐇵 *𝑻𝒊𝒕𝒖𝒍𝒐:* ${title}
-│𐇵 *𝐷𝑢𝑟𝑎𝑐𝑖𝑜𝑛:* ${publishedTime}
-│𐇵 *𝑉𝑖𝑠𝑡𝑎𝑠:* 2.422.839
-│𐇵 *𝐴𝑢𝑡𝑜𝑟:* Power Rangers para Niños - Canal Oficial 
-│𐇵 *𝐼𝐷:* zt0S82d9364
+│𐇵 *𝑃𝑢𝑏𝑙𝑖𝑎𝑑𝑜:* ${published}
+│𐇵 *𝐷𝑢𝑟𝑎𝑐𝑖𝑜𝑛:* ${duration}
+│𐇵 *𝑉𝑖𝑠𝑡𝑎𝑠:* ${views}
+│𐇵 *𝐴𝑢𝑡𝑜𝑟:* ${author}
+│𐇵 *𝐼𝐷:* ${id}
 │𐇵 *𝑇𝑖𝑝𝑜:* video
-│𐇵 *𝐸𝑛𝑙𝑎𝑐𝑒:* https://youtube.com/watch?v=zt0S82d9364
-│𐇵 *𝐶𝑎𝑛𝑎𝑙:* https://youtube.com/@PowerRangersNinosOficial
+│𐇵 *𝐸𝑛𝑙𝑎𝑐𝑒:* ${urls[0]}
+│𐇵 *𝐶𝑎𝑛𝑎𝑙:* [Canal del autor]
 ╰───────┈♢┈──────
 > *[ ℹ️ ] _𝐒𝐞 𝐞𝐬𝐭𝐚́ 𝐞𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐞𝐥 vídeo. 𝐞𝐬𝐩𝐞𝐫𝐞..._*`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: fkontak })
 await m.react(done)
 } catch {
 try {
 let yt = await fg.ytmp4(urls[0], q)
-let { title, size, dl_url } = yt
+let { title, size, dl_url, author, views, duration, published, id } = yt
 
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `🚩 El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `🚩 El archivo pesa más de ${limit} MB, se canceló la descarga.`, m, rcanal) 
 
 await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `_*DESCARGAS - MEGUMIN 🔥*_
 ╭───────┈♡┈──────
 │𐇵 *𝑻𝒊𝒕𝒖𝒍𝒐:* ${title}
-│𐇵 *𝐷𝑢𝑟𝑎𝑐𝑖𝑜𝑛:* 22m 43s
-│𐇵 *𝑉𝑖𝑠𝑡𝑎𝑠:* 2.422.839
-│𐇵 *𝐴𝑢𝑡𝑜𝑟:* Power Rangers para Niños - Canal Oficial 
-│𐇵 *𝐼𝐷:* zt0S82d9364
-│𐇵 *𝑇𝑖𝑝𝑂:* video
-│𐇵 *𝐸𝑛𝑙𝑎𝑐𝑒:* https://youtube.com/watch?v=zt0S82d9364
-│𐇵 *𝐶𝑎𝑛𝑎𝑙:* https://youtube.com/@PowerRangersNinosOficial
+│𐇵 *𝑃𝑢𝑏𝑙𝑖𝑎𝑑𝑜:* ${published}
+│𐇵 *𝐷𝑢𝑟𝑎𝑐𝑖𝑜𝑛:* ${duration}
+│𐇵 *𝑉𝑖𝑠𝑡𝑎𝑠:* ${views}
+│𐇵 *𝐴𝑢𝑡𝑜𝑟:* ${author}
+│𐇵 *𝐼𝐷:* ${id}
+│𐇵 *𝑇𝑖𝑝𝑜:* video
+│𐇵 *𝐸𝑛𝑙𝑎𝑐𝑒:* ${urls[0]}
+│𐇵 *𝐶𝑎𝑛𝑎𝑙:* [Canal del autor]
 ╰───────┈♢┈──────
 > *[ ℹ️ ] _𝐒𝐞 𝐞𝐬𝐭𝐚́ 𝐞𝐧𝐯𝐢𝐚𝐧𝐝𝐨 𝐞𝐥 vídeo. 𝐞𝐬𝐩𝐞𝐫𝐞..._*`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: fkontak })
 await m.react(done)
 } catch {
-await m.reply(`✘ *Ocurrío un error*`)
+await m.reply(`✘ *Ocurrió un error*`)
 }}}
 handler.help = ['Video']
 handler.tags = ['descargas', 'youtube']
-handler.customPrefix = /^(1|Video|video)/
-handler.command = new RegExp
-handler.register = true
-export default handler
-                                                    
+handler
+                                 
