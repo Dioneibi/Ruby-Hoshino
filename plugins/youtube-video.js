c15e341865f7b106fc16ba068163c9a364189a8d
Dioneibi <ulcidecono@gmail.com>
2024-09-23 00:40:53 -0400

    Update youtube-video.js

--- plugins/youtube-video.js
+++ plugins/youtube-video.js
@@ -1,94 +1,45 @@
-import fetch from "node-fetch"
-import yts from "yt-search"
+import fg from 'api-dylux'
+import yts from 'yt-search'
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+let limit = 350
+let handler = async (m, { conn, text, isPrems, isOwner, usedPrefix, command }) => {
+if (!m.quoted) return conn.reply(m.chat, '🚩 *Etiquete el mensaje que contenga el resultado del Play*', m, rcanal)
+if (!m.quoted.text.includes("*乂  Y O U T U B E  -  P L A Y  乂*")) return conn.reply(m.chat, '🚩 *Etiquete el mensaje que contenga el resultado del Play*', m, rcanal)
+if (!m.quoted.isBaileys) return conn.reply(m.chat, '🚩 Etiqueta el mensaje mío del resultado Play', m, rcanal)
+let urls = m.quoted.text.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
+if (!urls) return m.reply('×')
+if (urls.length < text) return conn.reply(m.chat, '🚩 *No se encontraron resultados*', m, rcanal)
+let q = urls[1] || '480p'
+try {
+await m.react(rwait)
+const yt = await fg.ytv(urls[0], q)
+let { title, dl_url, size } = yt 
 
-let handler = async (m, { conn, command, args, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `🚩 *Ingrese el nombre de un video de YouTube*\n\nEjemplo, !${command} Distancia - Kimberly Contreraxx`,  m, rcanal, )
-conn.reply(m.chat, global.wait, m, {
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `🚩 El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
+
+conn.reply(m.chat, `🕒 *Descargando El Video*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
 previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})
-try {
-await m.react(rwait)
-let yt_play = await search(args.join(" "))
-let img = await (await fetch(`${yt_play[0].image}`)).buffer()
-
-let txt = `*乂  Y O U T U B E  -  P L A Y  乂*\n\n`
-       txt += `✩ *𝐓𝐢𝐭𝐮𝐥𝐨:*\n${yt_play[0].title}\n\n`
-       txt += `✩ *𝐃𝐮𝐫𝐚𝐜𝐢𝐨𝐧:*\n${secondString(yt_play[0].duration.seconds)}\n\n`
-       txt += `✩ *𝐏𝐮𝐛𝐥𝐢𝐜𝐚𝐝𝐨 𝐄𝐧:*\n${yt_play[0].ago}\n\n`
-       txt += `✩ *𝐄𝐧𝐥𝐚𝐜𝐞:*\n${'https://youtu.be/' + yt_play[0].videoId}\n\n`
-       txt += `✨️ *Nota:* Para descargar responde a este mensaje con *1* o *2*.\n\n`
-       txt += `*1:* Video\n*2:* Audio`
-
-/* let txt = `・₊✧★。..・✫・🎸🎧°⋆♡₊˚ 🔮
-> 🌩 τιτυℓο:
-> • ${yt_play[0].title}
-> ◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈ ─๋︩︪───ׅ──ׅ─ׅ─ׅ┈ ─๋︩︪─◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸
-> 🌦 ρυϐℓιϲα∂ο єи: 
-> • ${yt_play[0].ago}
-> ◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎─ׅ─ׅ┈ ─๋︩︪───ׅ──ׅ─ׅ─ׅ┈ ─๋︩︪─⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸
-> 🍭 єиℓαϲє:
-> • ${yt_play[0].url}
-> ◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈ ─๋︩︪───ׅ──ׅ─ׅ─ׅ┈ ─๋︩︪─⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸
-> 🍒 αմԵօɾ:
-> • ${yt_play[0].author.name}
-> ◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈ ─๋︩︪──ׅ──ׅ──ׅ─ׅ┈ ─๋︩︪─◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸
-> 🧃 cαɳαℓ:
-> • ${yt_play[0].author.url}
-> ◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸─ׅ─ׅ┈ ─๋︩︪───ׅ──ׅ─ׅ─ׅ┈ ─๋︩︪─◌⃘࣭ٜ࣪࣪࣪۬☪︎︎︎︎̸
-> 🍇 ժմɾαcíօ́ղ:
-> • ${secondString(yt_play[0].duration.seconds)}
-・₊✧。..・★🎸🎧°⋆♡₊˚ 🔮
-
-> Para descargar responde a este mensaje con *1* o *2*.` */
-await conn.sendMessage(m.chat, {
-text: txt,
-contextInfo: { 
-forwardingScore: 9999, 
-isForwarded: true, 
-externalAdReply: {
-title: `${yt_play[0].title}`,
-body: dev,
-thumbnailUrl: img,
-thumbnail: img,
-sourceUrl: `${yt_play[0].url}`,
-mediaType: 1,
-renderLargerThumbnail: true
-}}}, { quoted: fkontak})
+await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `🚩 *Aquí está tu video de Youtube*\n${wm}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: fkontak })
 await m.react(done)
 } catch {
-await m.reply(`✘ Ocurrío un error`)}}
+try {
+let yt = await fg.ytmp4(urls[0], q)
+let { title, size, dl_url } = yt
 
-handler.help = ['play *<búsqueda>*', 'play2 *<busqueda>*']
+if (size.split('MB')[0] >= limit) return conn.reply(m.chat, `🚩 El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m, rcanal) 
+
+await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `🚩 *Aquí está tu video de Youtube*\n${wm}`, mimetype: 'video/mp4', fileName: `${title}` + `.mp4`}, {quoted: fkontak })
+await m.react(done)
+} catch {
+await m.reply(`✘ *Ocurrío un error*`)
+}}}
+handler.help = ['Video']
 handler.tags = ['descargas', 'youtube']
-handler.command = ['play', 'play2']
+handler.customPrefix = /^(1|Video|video)/
+handler.command = new RegExp
 handler.register = true
 export default handler
-
-async function search(query, options = {}) {
-let search = await yts.search({ query, hl: "es", gl: "ES", ...options });
-return search.videos;
-}
-
-function MilesNumber(number) {
-let exp = /(\d)(?=(\d{3})+(?!\d))/g;
-let rep = "$1.";
-let arr = number.toString().split(".");
-arr[0] = arr[0].replace(exp, rep);
-return arr[1] ? arr.join(".") : arr[0];
-}
-
-function secondString(seconds) {
-seconds = Number(seconds);
-var d = Math.floor(seconds / (3600 * 24));
-var h = Math.floor((seconds % (3600 * 24)) / 3600);
-var m = Math.floor((seconds % 3600) / 60);
-var s = Math.floor(seconds % 60);
-var dDisplay = d > 0 ? d + (d == 1 ? ":" : ":") : "";
-var hDisplay = h > 0 ? h + (h == 1 ? ":" : ":") : "";
-var mDisplay = m > 0 ? m + (m == 1 ? ":" : ":") : "";
-var sDisplay = s > 0 ? s + (s == 1 ? "" : "") : "";
-return dDisplay + hDisplay + mDisplay + sDisplay;
-}
