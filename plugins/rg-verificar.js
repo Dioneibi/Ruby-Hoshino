3517cc414ff6c131a5ac99d6ef1d437bc6ae5141
Dioneibi <ulcidecono@gmail.com>
2024-09-23 21:02:46 -0400

    Update rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -4,7 +4,10 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+
+
 const canales = 'https://chat.whatsapp.com/J7j9IlFhxbr809Urhu5KPJ';
+
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
