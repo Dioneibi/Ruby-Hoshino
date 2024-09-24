a3a64ade25f3579bd186187eb4d21a7e226fc802
Dioneibi <ulcidecono@gmail.com>
2024-09-24 10:49:19 -0400

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://api.github.com/repos/nevi67/VermeilBot-MD')
+let res = await fetch('https://api.github.com/repos/Dioneibi/Ruby-Hoshino')
 let json = await res.json()
 try {
 let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`
