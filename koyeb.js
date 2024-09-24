8b09eed5f02510a38aa3bd75c2802839ca423559
Dioneibi <ulcidecono@gmail.com>
2024-09-24 11:49:17 -0400

    Actualizar koyeb.js

--- koyeb.js
+++ koyeb.js
@@ -5,11 +5,11 @@ sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-rel
 sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
 sudo dnf clean all -y
 
-RUN git clone https://github.com/OfcDiego/LuffyBot-MD
+RUN git clone https://github.com/Dioneibi/Ruby-Hoshino
 
-WORKDIR /root/LuffyBot-MD
+WORKDIR /root/Ruby-Hoshino
 
-COPY ./root/LuffyBot-MD
+COPY ./root/Ruby-Hoshino
 
 RUN yarn install
 
