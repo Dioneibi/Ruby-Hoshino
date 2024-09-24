794205f51177a70542dad1009dc663c7782643c1
Dioneibi <ulcidecono@gmail.com>
2024-09-24 10:35:30 -0400

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -5,7 +5,7 @@ handler.all = async function(m) {
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, ` ¡Hola! Soy Vermeil, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, ` ¡Hola! Soy Ruby, en que puedo ayudarte hoy?\n\n✰ Usa *!menu* para ver mis comandos.`, m, rcanal, )
 }/*
 if (/^que|q$/i.test(m.text)) {
 conn.reply(m.chat, `*so y arroz* 👌🤌`, m, rcanal, )
