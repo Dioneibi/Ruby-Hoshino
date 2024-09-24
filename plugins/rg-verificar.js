065ce4ab95e29982f0e9865cdd275d2b487b4a63
Dioneibi <ulcidecono@gmail.com>
2024-09-23 21:08:16 -0400

    Update rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -1,42 +1,50 @@
-import db from '../lib/database.js'
 import { createHash } from 'crypto'
-import fs from 'fs'
-import fetch from 'node-fetch'
-
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
-
-
-const canal = 'https://chat.whatsapp.com/J7j9IlFhxbr809Urhu5KPJ';
-
 let handler = async function (m, { conn, text, usedPrefix, command }) {
-  let user = global.db.data.users[m.sender]
-  let name2 = conn.getName(m.sender)
-  if (user.registered === true) return m.reply(`💙 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`)
-  if (!Reg.test(text)) return m.reply(`💙 Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.16*`)
-  let [_, name, splitter, age] = text.match(Reg)
-  if (!name) return m.reply('💙 El nombre no puede estar vacío.')
-  if (!age) return m.reply('💙 La edad no puede estar vacía.')
-  if (name.length >= 100) return m.reply('💙 El nombre es demasiado largo.' )
-  age = parseInt(age)
-  if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
-  if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
-  user.name = name.trim()
-  user.age = age
-  user.regTime = + new Date
-  user.registered = true
-  let sn = createHash('md5').update(m.sender).digest('hex')
-  let img = await (await fetch(`https://tinyurl.com/2ckm7gda`)).buffer()
-  let txt = ` –  *R E G I S T R O  -  U S E R*\n\n`
-      txt += `┌  💙  *Nombre* : ${name}\n`
-      txt += `│  💙  *Edad* : ${age} años\n`
-      txt += `│  💙  *Numero de serie*\n`
-      txt += `└  💙  ${sn}`
-await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
-await m.react('✅')
+let user = global.db.data.users[m.sender]
+let name2 = conn.getName(m.sender)
+if (user.registered === true) throw `*『✦』Ya estas registrado, para volver a registrarte, usa el comando: #unreg*`
+if (!Reg.test(text)) throw `*『✦』El comando ingresado es incorrecto, uselo de la siguiente manera:*\n\n#reg *Nombre.edad*\n\n\`\`\`Ejemplo:\`\`\`\n#reg *VermeilBot-MD.18*`
+let [_, name, splitter, age] = text.match(Reg)
+if (!name) throw '*『✦』No puedes registrarte sin nombre, el nombre es obligatorio. Inténtelo de nuevo.*'
+if (!age) throw '*『✦』No puedes registrarte sin la edad, la edad es opcional. Inténtelo de nuevo.*'
+if (name.length >= 30) throw '*『✦』El nombre no debe de tener mas de 30 caracteres.*' 
+age = parseInt(age)
+if (age > 100) throw '*『😏』Viejo/a Sabroso/a*'
+if (age < 5) throw '*『🍼』Ven aquí, te adoptare!!*'
+user.name = name.trim()
+user.age = age
+user.regTime = + new Date
+user.registered = true
+global.db.data.users[m.sender].money += 600
+global.db.data.users[m.sender].estrellas += 10
+global.db.data.users[m.sender].exp += 245
+global.db.data.users[m.sender].joincount += 5
+let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
+m.react('📩') 
+let regbot = `👤 𝗥 𝗘 𝗚 𝗜 𝗦 𝗧 𝗥 𝗢 👤
+•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
+「💭」𝗡𝗼𝗺𝗯𝗿𝗲: ${name}
+「✨️」𝗘𝗱𝗮𝗱: ${age} años
+•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
+「🎁」𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:
+• 15 Estrellas 🌟
+• 5 vermeilCoins 🪙
+• 245 Experiencia 💸
+• 12 Tokens 💰
+•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
+• *magia:*
+• *tipo de magia :* no tiene
+• *mana :* 20/20
+• *rango :* f
+• *nivel :* 1
+•┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄•
+${packname}`
+await conn.sendLuffy(m.chat, '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰', textbot, regbot, imagen1, imagen1, canales, m)
+//await m.reply(`${sn}`)        
 }
-handler.help = ['reg'].map(v => v + ' *<nombre.edad>*')
+handler.help = ['reg']
 handler.tags = ['rg']
-
-handler.command = ['verify', 'reg', 'register', 'registrar'] 
+handler.command = ['verify', 'verificar', 'reg', 'register', 'registrar'] 
 
 export default handler
