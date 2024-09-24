9ae7f83fb3c2c185c8b4d9334e841a67c0a7f6d4
Dioneibi <ulcidecono@gmail.com>
2024-09-24 11:47:26 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -402,8 +402,8 @@ global.db.data.users[m.sender].spam = new Date * 1
 
 let hl = _prefix 
 let adminMode = global.db.data.chats[m.chat].modoadmin
-let luffy = `${plugins.botAdmin || plugins.admin || plugins.group || plugins || noPrefix || hl ||  m.text.slice(0, 1) == hl || plugins.command}`
-if (adminMode && !isOwner && !isROwner && m.isGroup && !isAdmin && vermeil) return   
+let Ruby = `${plugins.botAdmin || plugins.admin || plugins.group || plugins || noPrefix || hl ||  m.text.slice(0, 1) == hl || plugins.command}`
+if (adminMode && !isOwner && !isROwner && m.isGroup && !isAdmin && Ruby) return   
 if (plugin.rowner && plugin.owner && !(isROwner || isOwner)) { //número bot owner
 fail('owner', m, this)
 continue
