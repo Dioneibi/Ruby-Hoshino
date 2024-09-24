fb1c743f35bb19a2a424d96bf6b1a6dcba7e99c5
Dioneibi <ulcidecono@gmail.com>
2024-09-23 20:56:40 -0400

    Update rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -1,67 +1,38 @@
+import db from '../lib/database.js'
+import { createHash } from 'crypto'
+import fs from 'fs'
+import fetch from 'node-fetch'
+
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  
-  if (user.registered === true) throw `*『✦』Ya estás registrado, para volver a registrarte, usa el comando: #unreg*`
-  if (!Reg.test(text)) throw `*『✦』El comando ingresado es incorrecto, úselo de la siguiente manera:*\n\n#reg *Nombre.edad*\n\n\`\`\`Ejemplo:\`\`\`\n#reg *${name2}.18*`
-
+  if (user.registered === true) return m.reply(`💙 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`)
+  if (!Reg.test(text)) return m.reply(`💙 Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.16*`)
   let [_, name, splitter, age] = text.match(Reg)
-  
-  if (!name) throw '*『✦』No puedes registrarte sin nombre, el nombre es obligatorio. Inténtelo de nuevo.*'
-  if (!age) throw '*『✦』No puedes registrarte sin la edad, la edad es opcional. Inténtelo de nuevo.*'
-  if (name.length >= 30) throw '*『✦』El nombre no debe tener más de 30 caracteres.*' 
-  
+  if (!name) return m.reply('💙 El nombre no puede estar vacío.')
+  if (!age) return m.reply('💙 La edad no puede estar vacía.')
+  if (name.length >= 100) return m.reply('💙 El nombre es demasiado largo.' )
   age = parseInt(age)
-  
-  if (age > 999) throw '*『😏』¡Viejo/a Sabroso/a!*'
-  if (age < 5) throw '*¿𝐃𝐨𝐧𝐝𝐞 𝐞𝐬𝐭𝐚𝐧 𝐭𝐮𝐬 𝐩𝐚𝐩á𝐬?*😂'
-
+  if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
+  if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
   user.name = name.trim()
   user.age = age
-  user.regTime = +new Date
+  user.regTime = + new Date
   user.registered = true
-  global.db.data.users[m.sender].money += 600
-  global.db.data.users[m.sender].estrellas += 10
-  global.db.data.users[m.sender].exp += 245
-  global.db.data.users[m.sender].joincount += 5
-
-  let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)        
-  m.react('✅') 
-
-  let regbot = `╭─✦〘  𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗢 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗢 ✔️ 〙✦──╮
-┊
-┊🍚᷼⃞い》𝑁𝑜𝑚𝑏𝑟𝑒: ${name}
-┊🌸⃞ᰰ𝆆ᣞ 》𝐸𝑑𝑎𝑑: ${age} *Años*
-┊ ֪ ׂ🗡️ ̶ ׁ ֪ *>número de serie<*
-┊          ${sn}
-┊
-┊💻 *𝚁epo𝚂𝙸𝚃𝙾𝚁𝙸𝙾* *𝙶𝙸𝚃𝙷𝚄𝙱:* 
-┊https://github.com/Dioneibi/Ruby-Hoshino
-┊
-┊🥳 *¡Bienvenido/a al equipo!*
-┊🗂️ *Utiliza* ${usedPrefix}menu *para explorar los comandos disponibles.*
-┊
-┊✨ *𝗥𝗲𝗰𝗼𝗺𝗽𝗲𝗻𝘀𝗮𝘀:*
-┊15 🌟 Estrellas 
-┊ 5 🪙 yencoins
-┊ 245 💸 Experiencia
-┊ 12 💰 Tokens
-╰─────────────  ✦ ⁺.
-> 🎈 ¡Muchísimas gracias por usar a Ruby-Hoshino! 
-> Recuerda seguirme en mi canal para que no te pierdas nada de las novedades del bot. ¡Diviértete!`
-
-  conn.sendMessage(m.chat, {
-    text: regbot,
-    contextInfo: {
-      externalAdReply: {
-        title: '⊱『✅𝆺𝅥 𝗥𝗘𝗚𝗜𝗦𝗧𝗥𝗔𝗗𝗢(𝗔) 𝆹𝅥✅』⊰',
-        body: wm, 
-        thumbnailUrl: 'https://qu.ax/FGSG.jpg', 
-        sourceUrl: canales,
-        mediaType: 1,
-        showAdAttribution: true,
-        renderLargerThumbnail: true,
-      }
-    }
-  }, { quoted: fkontak })
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  let img = await (await fetch(`https://tinyurl.com/2ckm7gda`)).buffer()
+  let txt = ` –  *R E G I S T R O  -  U S E R*\n\n`
+      txt += `┌  💙  *Nombre* : ${name}\n`
+      txt += `│  💙  *Edad* : ${age} años\n`
+      txt += `│  💙  *Numero de serie*\n`
+      txt += `└  💙  ${sn}`
+await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
+await m.react('✅')
 }
+handler.help = ['reg'].map(v => v + ' *<nombre.edad>*')
+handler.tags = ['rg']
+
+handler.command = ['verify', 'reg', 'register', 'registrar'] 
+
+export default handler
