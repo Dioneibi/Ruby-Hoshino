919dcf2b4ee29339c4c4652595bc96924bde31cb
Dioneibi <ulcidecono@gmail.com>
2024-09-26 19:07:11 -0400

    Update main.comandos.js

--- plugins/main.comandos.js
+++ plugins/main.comandos.js
@@ -17,7 +17,7 @@ let handler = async (m, { conn }) => {
     // Función para enviar los comandos uno por uno con un intervalo de 5 segundos
     for (let i = 0; i < comandos.length; i++) {
         setTimeout(() => {
-            conn.reply(m.chat, `\`\`\`${comandos[i]}\`\`\``, m);
+            conn.reply(m.chat, ${comandos[i]}, m);
         }, i * 5000); // Intervalo de 5 segundos entre cada comando (5000 milisegundos)
     }
 }
